24733c3943f51580de7d657aa8561976d1c85f47
viki-patil <vikipatil1533@gmail.com>
2023-08-02 07:52:41 +0530

    Added else and elif

--- flaskpages.py
+++ flaskpages.py
@@ -20,18 +20,20 @@ def math_ops():
             r=num1+num2
             result="Addition of the "+str(num1)+" and "+str(num2)+" is "+str(r)
 
-        if ops =='subtract':
+        elif ops =='subtract':
             r=num1-num2
             result="Substraction of  "+str(num1)+"  and  "+str(num2)+"  is  "+str(r)
 
-        if ops == 'multiply':
+        elif ops == 'multiply':
             r=num1*num2
             result="Multiplication of the  "+str(num1)+ " and " +str(num2)+" is "+str(r)
 
-        if ops == 'divide':
+        elif ops == 'divide':
             r=int(num1/num2)
             result=f"Division of the  {num1} and {num2} is {r}"
-            
+
+        else:
+            result=f"{ops} is not a arithmatic operation or not Supported."    
 
         return render_template('results.html', result_in_html=result)
     
